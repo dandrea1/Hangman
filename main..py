3fd6554e377d10731e9871a0a922587a9e757f1d
dandrea1 <47754826+dandrea1@users.noreply.github.com>
2020-12-06 15:59:34 -0500

    updated the "you lose" print statement
    updated the "you lose" print statement to include what the random word was so that the player can see what word they missed.

--- main..py
+++ main..py
@@ -51,7 +51,7 @@ while not end_of_game:
             lives -= 1
             if lives == 0:
                  end_of_game = True
-                 print("You lose.")
+                 print("You lose.The word was {chosen_word}")
 
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
